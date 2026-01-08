8b70c394960d55d36cfc1872c1165d542d6ee565
Sagar1329 <sagargowda1329@gmail.com>
2026-01-08 23:54:53 +0530

    implement navigation

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className='text-3xl text-indigo-500'>page</div>
+    <div className='text-3xl text-indigo-500'></div>
   )
 }
 
---
+++ components/Header.tsx
@@ -0,0 +1,34 @@
+'use client'
+
+
+import { cn } from "@/lib/utils";
+import Image from "next/image"
+import Link from "next/link"
+import { usePathname } from "next/navigation";
+
+const Header = () => {
+    const pathname = usePathname();
+  return (
+    <header>
+    <div className="main-container inner">
+        <Link href="/">
+        <Image src='logo.svg' width={132} height={40} alt="Crypto Screener logo" />
+        </Link>
+
+        <nav>
+            <Link href="/" className={cn('nav-link',{
+                'is-active': pathname === '/',
+                'is-home':true
+            })}>Home</Link>
+            <p>Search Modal</p>
+              <Link href="/coins" className={cn('nav-link', {
+                  'is-active': pathname === '/coins',
+                  'is-home': true
+              })}>All coins</Link>
+        </nav>
+        </div>
+      </header>
+  )
+}
+
+export default Header
