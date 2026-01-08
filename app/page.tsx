0fcfce889e9e4814e83bc5780a0043a4c90dbd39
Sagar1329 <sagargowda1329@gmail.com>
2026-01-09 01:32:33 +0530

    implement Homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,123 @@
+import DataTable from '@/components/DataTable'
+import { cn } from '@/lib/utils';
+import { TrendingDown, TrendingUp } from 'lucide-react';
+import Image from 'next/image'
+import Link from 'next/link';
 import React from 'react'
 
+const dummyData: TrendingCoin[] = [
+  {item:{
+    id: '1',
+    name: 'Bitcoin',
+    symbol: 'BTC',
+    market_cap_rank: 1,
+    thumb: 'https://assets.coingecko.com/coins/images/1/small/bitcoin.png',
+    large: 'https://assets.coingecko.com/coins/images/1/large/bitcoin.png',
+    data: {
+      price: 10000,
+      price_change_percentage_24h: { usd: 0.5 },
+    },
+  }},
+  {item:{
+    id: '2',
+    name: 'Ethereum',
+    symbol: 'ETH',
+    market_cap_rank: 2,
+    thumb: 'https://assets.coingecko.com/coins/images/1/small/bitcoin.png',
+    large: 'https://assets.coingecko.com/coins/images/1/large/bitcoin.png',
+    data: {
+      price: 2000,
+      price_change_percentage_24h: { usd: -0.2 },
+    },
+  }},
+  {item:{
+    id: '3',
+    name: 'Litecoin',
+    symbol: 'LTC',
+    market_cap_rank: 3,
+    thumb: 'https://assets.coingecko.com/coins/images/1/small/bitcoin.png',
+    large: 'https://assets.coingecko.com/coins/images/1/large/bitcoin.png',
+    data: {
+      price: 500,
+      price_change_percentage_24h: { usd: 0.8 },
+    },
+  }},
+];
+const columns: DataTableColumn<TrendingCoin>[] = [
+  {header: 'Name',
+    cellClassName: 'name-cell',
+    cell: (coin) => {
+      const item = coin.item;
+      return (
+        <Link href={`/coins/${item.id}`} >
+          <Image src={item.large} alt={item.name}
+            width={36}
+            height={36}
+          />
+          <p>{item.name} </p>
+        </Link>
+      )
+    }
+
+  },
+   {header: '24h Change',
+    cellClassName: 'name-cell',
+    cell: (coin) => {
+      const item = coin.item;
+      const isTrendingUp = item.data.price_change_percentage_24h.usd > 0;
+      return (
+        <div className={cn('price-change', isTrendingUp ? 'text-green-500' : 'text-red-500')}>
+          <p>
+            {isTrendingUp ? 
+            (<TrendingUp width={16} height={16} />) :
+             (<TrendingDown width={16} height={16} />)}
+             {Math.abs(item.data.price_change_percentage_24h.usd).toFixed(2)}%
+          </p>
+
+
+        </div>
+      )
+    }
+  },
+  {
+    header:'Price',
+    cellClassName: 'price-cell',
+    cell: (coin) =>      coin.item.data.price
+
+  }
+
+
+
+
+
+]
 const page = () => {
-  return (
-    <div className='text-3xl text-indigo-500'></div>
-  )
+  return <main className='main-container'>
+    <section className='home-grid'>
+      <div id="coin-overview">
+        <div className='header'>
+          <Image src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png"
+            alt="Bitcoin"
+            width={56}
+            height={56}
+          />
+          <div className='info'>
+            <p> Bitcoin / BTC</p>
+            <h1>$92,113.00</h1>
+          </div>
+
+        </div>
+      </div><p>Trending coins</p>
+      <DataTable data={dummyData}
+       columns={columns}
+       rowKey={(coin)=> coin.item.id} 
+       tableClassName='trending-coins-table'/>
+    </section>
+
+    <section className='w-full mt-7 space-y-4'>
+      <p>Categories</p>
+    </section>
+  </main>
 }
 
 export default page
