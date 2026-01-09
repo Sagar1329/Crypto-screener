efb3b9a753e58dcf4916b086dbb2ab7e5f13bf4e
Sagar1329 <sagargowda1329@gmail.com>
2026-01-10 01:18:41 +0530

    implement coin overview and trending coins ui and api integration using fetcher utility function

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const formatCurrency = (amount: number): string => {
+  const formattedAmount = new Intl.NumberFormat('en-IN', {
+    style: 'currency',
+    currency: 'INR',
+  }).format(amount);
+
+  return formattedAmount;
+};
